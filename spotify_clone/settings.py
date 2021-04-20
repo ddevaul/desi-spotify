cfc3160090d1272ed15d8860cfd7603c29b24490
Desi <desi.devaul@gmail.com>
2021-04-20 15:24:47 -0400

    changed db

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lvn@--su6i63mwb6rpguv5q)fmp3%ul6kt=hy+g=*g#n#c%#gb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['desi-spotify.herokuapp.com', 'localhost']
-
 
 # Application definition
 
@@ -74,13 +73,15 @@ WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
